f87b5c423d8ccac86756a391f51e1f30828ecdc4
Clemens Stich <clemensstich@googlemail.com>
2025-09-21 22:01:23 +0200

    - Added infra code for support bot

---
+++ infrastructure/lib/CommonUtils.ts
@@ -0,0 +1,43 @@
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class CommonUtils {
+  private readonly parentStack: cdk.Stack;
+
+  constructor(parentStack: cdk.Stack) {
+    this.parentStack = parentStack;
+  }
+
+  public allowParameterStoreAccess(
+    lambdaFunctions: Array<lambda.Function>,
+    paramStorePathPrefix: string
+  ): iam.PolicyStatement[] {
+    const region = this.parentStack.region;
+    const accountId = this.parentStack.account;
+
+    const actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+    ];
+    const ssmPolicy = new iam.PolicyStatement({
+      actions,
+      resources: [
+        `arn:aws:ssm:${region}:${accountId}:parameter${paramStorePathPrefix}`,
+      ],
+    });
+
+    const kmsPolicy = new iam.PolicyStatement({
+      actions: ["kms:Decrypt", "kms:Encrypt"],
+      resources: [`arn:aws:kms:${region}:${accountId}:key/*`],
+    });
+
+    for (let lambdaFunc of lambdaFunctions) {
+      lambdaFunc.addToRolePolicy(ssmPolicy);
+      lambdaFunc.addToRolePolicy(kmsPolicy);
+    }
+
+    return [ssmPolicy, kmsPolicy];
+  }
+}
