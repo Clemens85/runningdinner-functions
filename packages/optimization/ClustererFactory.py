c91a74d1a74b05c19bf1918cc7eb44a8d8909c5f
Clemens Stich <clemensstich@googlemail.com>
2026-03-01 22:33:44 +0100

    - use new clusterer in ClustererFactory.py

--- packages/optimization/ClustererFactory.py
+++ packages/optimization/ClustererFactory.py
@@ -1,9 +1,9 @@
 from Clusterer import Clusterer
-from ClustererNewMealAssignmentsV2 import ClustererNewMealAssignmentsV2
+from ClustererNewMealAssignments import ClustererNewMealAssignments
 from DataProvider import DataProvider
 from DefaultClusterer import DefaultClusterer
 
 def get_clusterer_instance(data_provider: DataProvider) -> Clusterer:
     if data_provider.get_optimization_settings().ignoreMealAssignments:
-        return ClustererNewMealAssignmentsV2(data_provider=data_provider)
+        return ClustererNewMealAssignments(data_provider=data_provider)
     return DefaultClusterer(data_provider=data_provider)
